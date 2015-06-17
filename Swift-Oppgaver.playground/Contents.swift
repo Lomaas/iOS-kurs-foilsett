fcbe2d64c30e190cf7a14836f0dbb8506c4d403b
Aleksander Hindenes <alekshindenes@hotmail.com>
2015-06-17 23:08:05 +0200

    add oppgaver i Swift

---
+++ Swift-Oppgaver.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+
+// Oppgave 1: Lag en funksjon som skriver ut alle prosjektrollene
+let prosjektroller = ["CSS-guru", "Designer", "Backend", "Frontend", "Full stack"]
+
+
+
+// Oppgave 2: Lag en funksjon som sjekker om et heltall er et partall
+
+
+
+// Oppgave 3: Lag en funksjon som tar inn en liste av tall og
+// returnerer en liste der bare partallene er med (bruk funksjonen fra forrige oppgave)
+// a - implementer ved å appende partall til en liste
+// b - implementer ved å bruke Array sin .filter()
+let randomTall = [arc4random(), arc4random(), arc4random(), arc4random(), arc4random(), arc4random()]
+
+
+
+// Oppgave 4: Bruk Array sin .sort til å sortere tallene under i stigende rekkefølge.
+// Steg 2: ta i bruk $-syntaks
+let tall = [1, 3, 2, 4, 6, 10, 8, 9, 7, 5]
+
+
+
+// Oppgave 5: Lag en funksjon "hellom" som tar inn et navn av typen Optional String hvor
+// hello("Aleksander") returnerer "Hello Aleksander"
+// hello(nil) returnerer "Hello World"
+
+
+
+// Oppgave 6: Lag en funksjon som dividerer to heltall.
+// Funksjonen bør returnere nil/.None hvis nevneren er 0
+
+
+
+// Oppgave 7: Lag en extension til Array som tilbyr forEach funksjonalitet - altså kaller en funksjon for hvert element i listen.
+// Se Swift Documentation -> Language Guide for info om extensions
+
+
+
+
+// Oppgave 8: Bruk Language Guide til å lese om og utforske mer om Swift
+// Forslag: "Classes and Structs", "Optional Chaining", "Automatic Reference Counting"
